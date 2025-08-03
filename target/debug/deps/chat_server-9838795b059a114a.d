dce806d5edfb4e0220b5c0bd5aed75135e5bd94e
Muktar Sadiq <muktarsadiq.m@yahoo.com>
2025-08-03 14:03:23 +0100

    Remove target directory from tracking

--- target/debug/deps/chat_server-9838795b059a114a.d
+++
@@ -1,5 +0,0 @@
-/Users/user/Desktop/chat_server/target/debug/deps/chat_server-9838795b059a114a.d: src/main.rs
-
-/Users/user/Desktop/chat_server/target/debug/deps/libchat_server-9838795b059a114a.rmeta: src/main.rs
-
-src/main.rs:
